318b7385002dfdca51d075c69e285e2fa46ca7db
Anju <anjumpkr4115@gmail.com>
2024-10-25 19:36:03 +0530

    hosting

--- config/google.mjs
+++ config/google.mjs
@@ -26,6 +26,7 @@ passport.use(
            })
                await user.save()
            }
+           req.session.isUser= email;
                done(null, user)
            } catch (err) {
                console.log(`error during google authentication: ${err}`)
